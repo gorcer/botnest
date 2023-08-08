c224a1e010a68a039e388521bcbb91fb9d5ad42d
Egor <gorcer@gmail.com>
2023-08-08 23:06:26 +1000

    fix

--- src/bot/lonelyTrader.service.ts
+++ src/bot/lonelyTrader.service.ts
@@ -8,6 +8,7 @@ import { BalanceService } from "../balance/balance.service";
 import { BalancesDto } from "../balance/dto/balances.dto";
 import { OrderService } from "../order/order.service";
 import { OrderSideEnum } from "../order/entities/order.entity";
+import { ApiService } from "../exchange/api.service";
 
 const { divide, subtract, multiply, compareTo, add } = require("js-big-decimal");
 
@@ -26,7 +27,7 @@ export class LonelyTraderService {
 
 	accountId = 1;
 	config: Config;
-	api: { getActualRates: (arg0: string) => PromiseLike<{ bid: any; ask: any; }> | { bid: any; ask: any; }; fetchBalances: () => BalancesDto | PromiseLike<BalancesDto>; getLastPrice: (arg0: string) => any; };
+	api:ApiService;
 	accountConfig: { orderProbability: number; };
 
 	constructor(
@@ -48,33 +49,33 @@ export class LonelyTraderService {
 			balanceSync: process.env.BOT_BALANCE_SYNC == 'true'
 		}
 
+
+		const overalAccountConf = {
+			pair: process.env.BOT_CURRENCY1 + '/' + process.env.BOT_CURRENCY2,
+			orderAmount: Number(process.env.BOT_ORDER_AMOUNT),
+			currency1: process.env.BOT_CURRENCY1,
+			currency2: process.env.BOT_CURRENCY2,
+			orderProbability: Number(process.env.BOT_ORDER_PROBABILITY),
+		};
+
 		if (process.env.BOT_TEST == 'true') {
 			this.accounts.setAccount(this.accountId, {
-				exchangeName: process.env.EXCHANGE_NAME,
-				apiKey: process.env.EXCHANGE_TESTNET_API_KEY,
-				secret: process.env.EXCHANGE_TESTNET_API_SECRET,
-				isSandbox: true,
-
-				pair: process.env.TESTBOT_CURRENCY1 + '/' + process.env.TESTBOT_CURRENCY2,
-				orderAmount: Number(process.env.TESTBOT_ORDER_AMOUNT),
-				currency1: process.env.TESTBOT_CURRENCY1,
-				currency2: process.env.TESTBOT_CURRENCY2,
-				orderProbability: Number(process.env.TESTBOT_ORDER_PROBABILITY),
-
+				...overalAccountConf, ...{
+					exchangeName: process.env.EXCHANGE_NAME,
+					apiKey: process.env.EXCHANGE_TESTNET_API_KEY,
+					secret: process.env.EXCHANGE_TESTNET_API_SECRET,
+					isSandbox: true,
+				}
 			});
 		} else {
 			this.accounts.setAccount(this.accountId, {
-				exchangeName: process.env.EXCHANGE_NAME,
-				apiKey: process.env.EXCHANGE_API_KEY,
-				secret: process.env.EXCHANGE_API_SECRET,
-				isSandbox: false,
+				...overalAccountConf, ...{
+					exchangeName: process.env.EXCHANGE_NAME,
+					apiKey: process.env.EXCHANGE_API_KEY,
+					secret: process.env.EXCHANGE_API_SECRET,
+					isSandbox: false,
 
-				pair: process.env.BOT_CURRENCY1 + '/' + process.env.BOT_CURRENCY2,
-				orderAmount: Number(process.env.BOT_ORDER_AMOUNT),
-				currency1: process.env.BOT_CURRENCY1,
-				currency2: process.env.BOT_CURRENCY2,
-				orderProbability: Number(process.env.BOT_ORDER_PROBABILITY),
-
+				}
 			});
 		}
 
@@ -93,20 +94,17 @@ export class LonelyTraderService {
 
 	async trade() {
 
-
 		const lastOrder = await this.orders.getLastOrder(this.accountId);
-		let startRate=1;
-		if (lastOrder.side == OrderSideEnum.BUY) {
+		let startRate = 1;
+		if (lastOrder && lastOrder.side == OrderSideEnum.BUY) {
 			startRate = lastOrder.rate;
 		}
 		let
 			lastAsk: number = startRate,
 			lastBid: number = startRate,
 			lastStatUpdate = 0,
-			lastTradesUpdate = Date.now()/1000,
+			lastTradesUpdate = Date.now() / 1000,
 			syncStatus = false;
-
-
 
 
 		while (!syncStatus) {
@@ -135,12 +133,12 @@ export class LonelyTraderService {
 
 				if (elapsedSecondsFrom(SEC_IN_HOUR, lastStatUpdate)) {
 					await this.saveStat(this.accountId, this.config.currency1, this.config.currency2);
-					lastStatUpdate = Date.now()/1000;
+					lastStatUpdate = Date.now() / 1000;
 				}
 
 				if (elapsedSecondsFrom(SEC_IN_HOUR, lastTradesUpdate)) {
 					await this.checkCloseOrders();
-					lastTradesUpdate = Date.now()/1000;
+					lastTradesUpdate = Date.now() / 1000;
 				}
 
 				const { bid: rateBid, ask: rateAsk } = await this.api.getActualRates(this.config.pair);
